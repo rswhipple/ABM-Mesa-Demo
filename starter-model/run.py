ac9b35e78c962b0d20d7f4b99e9513c9f50e4d5b
Rebecca W Silverstein <rswhipple@gmail.com>
2024-07-03 15:47:31 -0700

    add: run logic and visualizer

--- starter-model/run.py
+++ starter-model/run.py
@@ -1,4 +1,27 @@
+import seaborn as sns
 from money_model import MoneyModel
 
-starter_model = MoneyModel(10)
-starter_model.step()
+model = MoneyModel(10)
+
+agent_wealth = [a.wealth for a in model.schedule.agents]
+# Create a histogram with seaborn
+g = sns.histplot(agent_wealth, discrete=True)
+g.set(
+    title="Wealth distribution", xlabel="Wealth", ylabel="Number of agents"
+);  # The semicolon is just to avoid printing the object representation
+
+all_wealth = []
+# This runs the model 100 times, each model executing 10 steps.
+for j in range(100):
+    # Run the model
+    model = MoneyModel(10)
+    for i in range(10):
+        model.step()
+
+    # Store the results
+    for agent in model.schedule.agents:
+        all_wealth.append(agent.wealth)
+
+# Use seaborn
+g = sns.histplot(all_wealth, discrete=True)
+g.set(title="Wealth distribution", xlabel="Wealth", ylabel="Number of agents");
