205cd2bbe93b4cdd592c9e07469ef5d955244c4c
Rebecca W Silverstein <rswhipple@gmail.com>
2024-07-03 15:19:53 -0700

    add: scheduler

--- starter-model/money-model.py
+++ starter-model/money-model.py
@@ -19,13 +19,29 @@ class MoneyAgent(mesa.Agent):
 
         # Create the agent's variable and set the initial values.
         self.wealth = 1
+    
+    def step(self):
+        # The agent's step will go here.
+        # For demonstration purposes we will print the agent's unique_id
+        print(f"Hi, I am an agent, you can call me {str(self.unique_id)}.")
 
 class MoneyModel(mesa.Model):
     """A model with some number of agents."""
-
+    
     def __init__(self, N):
         super().__init__()
         self.num_agents = N
+        # Create scheduler and assign it to the model
+        self.schedule = mesa.time.RandomActivation(self)
+
         # Create agents
         for i in range(self.num_agents):
             a = MoneyAgent(i, self)
+            # Add the agent to the scheduler
+            self.schedule.add(a)
+    
+    def step(self):
+        """Advance the model by one step."""
+
+        # The model's step will go here for now this will call the step method of each agent and print the agent's unique_id
+        self.schedule.step()
